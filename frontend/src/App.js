88e841b226eb4de9708c4a17adacaaa467964f8f
ridoineel <ridoineelofficiel@gmail.com>
2022-04-15 09:18:49 +0000

    fix markdown initial loading bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,30 +9,34 @@ import {marked} from "marked";
 // const dompurify = createDomPurify(new JSDOM().window);
 
 function App() {
-  let [markdown, setMarkdown] = useState(null);
-  let [html, setHtml] = useState(null);
+  let [markdown, setMarkdown] = useState("");
+  let [html, setHtml] = useState("");
 
   useEffect(() => {
-
     let markD = localStorage.getItem("contentMarkdown");
 
-    setMarkdown(markD)
+    setMarkdown(markD || "")
   }, [])
 
   useEffect(() => {
     // convert markdown to html
+    let htmlContent
+
     if (markdown) {
-      let htmlContent = marked(markdown) // dompurify.sanitize(marked(markdown))
-
-      localStorage.setItem("contentMarkdown", markdown);
-
-      setHtml(htmlContent)
+      htmlContent = marked(markdown) // dompurify.sanitize(marked(markdown))
+    }else { 
+      htmlContent = "Html output..."
     }
+
+    // save markdown in local storage
+    localStorage.setItem("contentMarkdown", markdown);
     
+    // update html text
+    setHtml(htmlContent)
   }, [markdown])
 
   return (
-    <div className="App min-height-full container">
+    <div className="App min-height-full">
 
       <div className="ide-container">
         <MarkdownPane content={markdown} setMarkdown={setMarkdown}/>
