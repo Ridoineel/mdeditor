f19dc7fd0f5c96917c4ee7b30db700bad83c619e
ridoineel <ridoineelofficiel@gmail.com>
2022-04-18 12:15:22 +0000

    update.. git add config-overrides.js  bug not fixed at this point

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -2,20 +2,11 @@ module.exports = function override (config, env) {
     let loarders = config.resolve
     
     loarders.fallback = {
-        "net": false,
-        "tls": false,
         "fs": false,
-        "child_process": false,
-        "url": require.resolve("url/"),
-        "assert": require.resolve("assert/"),
+        "process": false,
         "path": require.resolve("path-browserify"),
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "stream": require.resolve("stream-browserify"),
-        "zlib": require.resolve("browserify-zlib"),
-        "buffer": require.resolve("buffer/"), 
-        "util": require.resolve("util/"),
-        "os": require.resolve("os-browserify/browser")
+        "os": require.resolve("os-browserify/browser"),
+        
     }
 
     return config
